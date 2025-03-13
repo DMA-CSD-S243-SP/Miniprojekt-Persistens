2b448003be4e3db75111928b05208fd7f021b2f3
Line-B-333 <10187764@ucn.dk>
2025-03-13 11:02:57 +0100

    Update: ProductDaoImpl
    addded authour

--- src/database/ProductDaoImpl.java
+++ src/database/ProductDaoImpl.java
@@ -3,6 +3,12 @@ package database;
 import java.util.List;
 import model.Product;
 
+/**
+ * 
+ * @author Line Bertelsen
+ * @date 13.02.25 - 11.01
+ */
+
 public interface ProductDaoImpl
 {
 	List <Product> findAllProducts() throws DataAccessException; 

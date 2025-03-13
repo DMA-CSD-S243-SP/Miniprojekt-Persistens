e34a879478d57cf7e18f50353d3a47e45cabd124
LumiUCN <10632106@ucn.dk>
2025-03-13 10:57:37 +0100

    Create EmployeeDaoImpl.java

---
+++ src/database/EmployeeDaoImpl.java
@@ -0,0 +1,42 @@
+package database;
+
+import java.util.List;
+
+import model.Employee;
+
+/**
+ * Interface responsible for dictating how the Employee table can be accessed.
+ * 
+ * @author Lumière Schack
+ */
+public interface EmployeeDaoImpl
+{
+	/**
+	 * Method creates a list containing a shallow clone of every employee in the
+	 * database. If fullAssociation is false then associations will only be filled
+	 * with information found in the respective row of each employee in the Employee
+	 * table. Otherwise associations will be filled in with the information from the
+	 * row that the employee has a key to.
+	 * 
+	 * @param fullAssociation - Whether the Employees' associations should be looked
+	 *                        up in the database.
+	 * @return A list containing a shallow clone of every Employee in the database.
+	 */
+	List<Employee> findAllEmployees(boolean fullAssociation) throws DataAccessException;
+
+	/**
+	 * Method creates a shallow clone of the Employee that has the given id. If no
+	 * matching employee is found the method returns null. If fullAssociation is
+	 * false then associations will only be filled with information found in the
+	 * respective row of the employee in the Employee table. Otherwise associations
+	 * will be filled in with the information from the row the that employee has a key
+	 * to.
+	 * 
+	 * @param employeeId      - The id used for the querry.
+	 * @param fullAssociation - Whether the Employee's associations should be looked
+	 *                        up in the database.
+	 * @return A shallow clone of the Employee that has an id that matches the
+	 *         querry or NULL if no match is found.
+	 */
+	public Employee findEmployeeById(String employeeId, boolean fullAssociation);
+}

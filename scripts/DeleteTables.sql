fab6f1a5fc7ce09473f6769abe49546590803e4b
LumiUCN <10632106@ucn.dk>
2025-03-14 12:53:24 +0100

    Create DeleteTables.sql

---
+++ scripts/DeleteTables.sql
@@ -0,0 +1,18 @@
+Drop Table [Works at]
+Drop Table Licenseplate
+Drop Table Cd
+Drop Table Equipment
+Drop Table Clothing
+Drop Table GunReplica
+Drop Table SaleOrderLine
+Drop Table Product
+Drop Table SaleOrder
+Drop Table StockKeepingUnit
+Drop Table Customer
+Drop Table Employee
+Drop Table Invoice
+Drop Table Warehouse
+Drop Table Store
+Drop Table Supplier
+Drop Table PostalCode
+Drop Table Country

6935a7c8c1991354b42258a923144a1713e359f0
LumiUCN <10632106@ucn.dk>
2025-03-14 11:20:31 +0100

    Create TestData.sql

---
+++ scripts/TestData.sql
@@ -0,0 +1,17 @@
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (1, 'Cowboyhat - Sandfarvet', 200.00, 250.00, 'USA', 20)
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (2, 'Cowboyhat - Krokodilleskind', 225.00, 251.00, 'USA', 20);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (3, 'Cowboyhat - Mørk', 120.00, 200.00, 'USA', 20);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (4, 'Cowboyhat - Enhjørningskind', 225.00, 1500.00, 'USA', 20);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (5, 'Realistisk Revolver - 50 caliber', 50.00, 750.00, 'Ungarn', 15);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (6, 'Cowboystøvler - Brunt Læder', 90.00, 251.00, 'Polen', 30);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (7, 'Cowboystøvler - Sort Læder', 90.00, 500.00, 'Polen', 30);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (8, 'Hælsporre', 40.00, 200.00, 'Ungarn', 30);
+INSERT INTO Product (id, name, purchasePrice, salesPrice, countryOfOrigin, minStock) VALUES (9, 'Buffaloslips', 60.00, 100.00, 'USA', 40);
+
+INSERT INTO Employee (id, firstName, lastName, title, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (1000, 'Jens', 'Hansen', 'Manager', '12345678', 'jens@example.com', 'Hovedstaden',  1000, 'Strøget', 10, 2, 'A');
+INSERT INTO Employee (id, firstName, lastName, title, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (1001, 'Marie', 'Nielsen', 'Ekspedient', '87654321', 'marie@example.com',  'Midtjylland', 8000, 'Åboulevarden', 20, 3, 'B');
+INSERT INTO Employee (id, firstName, lastName, title, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (1002, 'Anders', 'Larsen', 'Ekspedient', '11223344', 'anders@example.com',  'Syddanmark', 5000, 'Vestergade', 5, 1, 'C');
+
+INSERT INTO Customer (id, firstName, lastName, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (101, 'Mikkel', 'Poulsen', '22334455', 'mikkel@example.com', 'Hovedstaden', 2000,'Gammel Kongevej', 15, 1, '1A');
+INSERT INTO Customer (id, firstName, lastName, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (102, 'Sofie', 'Jørgensen', '33445566', 'sofie@example.com', 'Nordjylland', 9000, 'Boulevarden', 25, 2, '2B');
+INSERT INTO Customer (id, firstName, lastName, phoneNumber, emailAddress, stateName, postalCode, streetName, houseNumber, floorNumber, doorNumber) VALUES (103, 'Rasmus', 'Madsen', '44556677', 'rasmus@example.com', 'Sjælland', 4000, 'Algade', 30, 4, '3C');

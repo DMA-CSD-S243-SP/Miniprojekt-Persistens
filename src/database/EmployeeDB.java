96ca0f3a6103e5f67d4eac7b35dcb484440b4750
ManifestManah <ManifestManah@users.noreply.github.com>
2025-03-19 19:34:05 +0100

    Changed: EmployeeDB
    Made changes to existing comments and added comments to the entire flow of the EmployeeDBclass for clarification of how these types of DB classes works in their entirety.

--- src/database/EmployeeDB.java
+++ src/database/EmployeeDB.java
@@ -9,108 +9,181 @@ import java.util.List;
 
 import model.Employee;
 
-
 /**
- * This class is responsible for accessing 
- * and managing employee objects stored in a database.
+ * This class is responsible for accessing and managing employee objects stored
+ * in a database.
  * 
- * It implements the employeeDapImpl, 
- * meaning its implements its methods
+ * It implements the employeeDapImpl, meaning its implements its methods
  * 
- * @author Anders Have
- * @version 13/03/2025 - 13:45
+ * @author Anders Have & Christoffer Søndergaard
+ * @version 19/03/2025 - 19:33
  */
-public class EmployeeDB implements EmployeeDaoImpl 
+public class EmployeeDB implements EmployeeDaoImpl
 {
-	// this selects all the colums in the employee tabel
-	private static final String FIND_ALL_QUERIES = "SELECT id, firstName, lastName, title, phoneNumber, emailAddress, streetName,"
-			+ "streetName, houseNumber, floorNumber, doorNumber, stateName, postalCode from Employee";
-	// this builds on FIND_ALL_QUERIES by adding a filter and a placeholder.
+	// Selects all of the data within the Employee table in the database
+	private static final String FIND_ALL_QUERIES = "SELECT id, firstName, lastName, title, phoneNumber, emailAddress, streetName, streetName, houseNumber, floorNumber, doorNumber, stateName, postalCode from Employee";
+
+	// This extends on the FIND_ALL_QUERIES by adding a filtering condition with a placeholder for employee id
 	private static final String FIND_ALL_EMPLOYEEID_QUERY = FIND_ALL_QUERIES + " where id = ?";
+
+	// PreparedStatement for retrieving all employees from the database
+	private PreparedStatement findAllEmployee;
 	
-	private PreparedStatement findAllEmployee; 
+	// PreparedStatement for retrieving an employee based on their email address
 	private PreparedStatement findByEmployeeId;
-	
-	
-	public EmployeeDB() throws SQLException 
-	{
-		findAllEmployee = DBConnection.getInstance().getConnection()
-				.prepareStatement(FIND_ALL_QUERIES);
-		findByEmployeeId = DBConnection.getInstance().getConnection()
-				.prepareStatement(FIND_ALL_EMPLOYEEID_QUERY);
-	}
-	
-	
+
 	/**
+	 * Constructor for EmployeeDB.
+	 * Initializes prepared statements for executing SQL queries.
 	 * 
+	 * @throws SQLException if there is an issue with the database connection
 	 */
+	public EmployeeDB() throws SQLException
+	{
+		// Prepares the SQL statement for retrieving all employees
+		findAllEmployee = DBConnection.getInstance().getConnection().prepareStatement(FIND_ALL_QUERIES);
+		
+		// Prepares the SQL statement for retrieving an Employee by their employee id
+		findByEmployeeId = DBConnection.getInstance().getConnection().prepareStatement(FIND_ALL_EMPLOYEEID_QUERY);
+	}
+
+	
+    /**
+     * Retrieves all employees from the database.
+     * 
+     * @return a list of all employees
+     * @throws DataAccessException if retrieval fails
+     */
 	@Override
 	public List<Employee> findAllEmployees(boolean fullAssociation) throws DataAccessException
 	{
-		Connection con = DBConnection.getInstance().getConnection();
-		try 
+		// Gets a connection to the database
+		Connection databaseConnection = DBConnection.getInstance().getConnection();
+		
+		try
 		{
-			findAllEmployee = con.prepareStatement(FIND_ALL_QUERIES);
-			ResultSet resultSet = findAllEmployee.executeQuery();
-			List<Employee> res = buildObjects(resultSet);
-			return res;
-		} catch (SQLException e) 
+			// Prepare a SQL statement to retrieve all employees
+			findAllEmployee = databaseConnection.prepareStatement(FIND_ALL_QUERIES);
+			
+			// Executes the prepared statement and stores the result set
+			ResultSet employeeResultSet = findAllEmployee.executeQuery();
+			
+			// Converts the result set into a list of Employee objects
+			List<Employee> reslistOfEmployees = buildObjects(employeeResultSet);
+			
+			// Returns the list of Employee objects
+			return reslistOfEmployees;
+		}
+		
+		catch (SQLException exception)
 		{
-			throw new DataAccessException("Could not find all", e);
+			// If an SQL error occurs a custom exception is thrown with the specified details
+			throw new DataAccessException("Unable to find Employee objects in the database", exception);
 		}
 	}
 
-
+	
+    /**
+     * Finds an Employee object by searching for an employee with a matching employee id.
+     * 
+     * @param employeeId the id of the employee to search for
+     * @return the corresponding Employee object, or null if not found
+     * @throws DataAccessException if retrieval fails
+     */
 	@Override
 	public Employee findEmployeeById(int employeeId, boolean fullAssociation) throws DataAccessException
 	{
-		Connection con = DBConnection.getInstance().getConnection();
-		try {
-			findByEmployeeId = con.prepareStatement(FIND_ALL_EMPLOYEEID_QUERY);
+		// Gets a connection to the database
+		Connection databaseConnection = DBConnection.getInstance().getConnection();
+		
+		try
+		{
+			// Prepares a SQL statement to find and retrieve an Employee with a matching employee id
+			findByEmployeeId = databaseConnection.prepareStatement(FIND_ALL_EMPLOYEEID_QUERY);
+			
+			// Adds the employee id provided in the method's parameter to the String instead of the placeholder
 			findByEmployeeId.setInt(1, employeeId);
+			
+			// Executes the query, and stores the retrieved data in the variable named resultSet
 			ResultSet resultSet = findByEmployeeId.executeQuery();
-			Employee em = null;
-			if(resultSet.next()) 
+			
+			// Creates and initializes an Employee object as null, which will later be populated with Employee specific data
+			Employee employee = null;
+			
+			// Iterates through the resultSet while there are still more rows in the database's table
+			if (resultSet.next())
 			{
-				em = buildObject(resultSet);
+				// Converts the retrieved database row into an Employee object using the buildObject method
+				employee = buildObject(resultSet);
 			}
-			return em;
-		} 
-		catch (SQLException e) 
-		{
-			throw new DataAccessException("Could not find by id = " + employeeId, e);
-		}	
+			
+			// Returns the employee with a matching employee id or null if no employee has the specified employee id
+			return employee;
 		}
+		
+		catch (SQLException exception)
+		{
+			// If an SQL error occurs a custom exception is thrown with the specified details
+			throw new DataAccessException("Unable to find an Employee object with an employee id matching: " + employeeId, exception);
+		}
+	}
+
 	
+    /**
+     * Builds a specific Employee object from a database result set.
+     * 
+     * @param resultSet the result set containing Employee data
+     * @return an Employee object with the extracted data
+     * @throws SQLException if accessing the result set fails
+     */
 	private Employee buildObject(ResultSet resultSet) throws SQLException
-		{
-			Employee em = new Employee(
-					resultSet.getInt("id"),
-					resultSet.getString("firstName"),
-					resultSet.getString("lastName"),
-					resultSet.getString("title"),
-					resultSet.getString("phoneNumber"),
-					resultSet.getString("emailAddress"),
-					/*resultSet.getString("country")*/ null,
-					resultSet.getString("stateName"),
-					/*resultSet.getString("city")*/ null,
-					resultSet.getInt("postalCode"),
-					resultSet.getString("streetName"),
-					resultSet.getInt("houseNumber"),
-					resultSet.getInt("floorNumber"),
-					resultSet.getString("doorNumber")
-					);
-			return em;
-		}
+	{
+		// Creates a Employee object stored within the employee variable based off of the method's provided resultSet
+		Employee employee = new Employee(
+				resultSet.getInt("id"),
+				resultSet.getString("firstName"),
+				resultSet.getString("lastName"),
+				resultSet.getString("title"),
+				resultSet.getString("phoneNumber"),
+				resultSet.getString("emailAddress"),
+				// country - We supply null here, as country can be deducted from the supplied State, hence no need to waste storage within the database for this
+				null,
+				
+				resultSet.getString("stateName"),
+				
+				// city - We supply null here, as country can be deducted from the supplied Postal Code, hence no need to waste storage within the database for this
+				null,
+				resultSet.getInt("postalCode"),
+				resultSet.getString("streetName"),
+				resultSet.getInt("houseNumber"),
+				resultSet.getInt("floorNumber"),
+				resultSet.getString("doorNumber")
+				);
+		
+		return employee;
+	}
 
-
+	
+    /**
+     * Converts a result set into a list of Employee objects.
+     * 
+     * @param resultSet the result set containing multiple Employee records
+     * @return a list of Employee objects
+     * @throws SQLException if accessing the result set fails
+     */
 	private List<Employee> buildObjects(ResultSet resultSet) throws SQLException
 	{
+		// Creates an empty list to store Employee objects within
 		List<Employee> em = new ArrayList<>();
-		while(resultSet.next()) 
+		
+		// Iterates through the result set while there are still more rows in the database's table
+		while (resultSet.next())
 		{
+			// Converts each row into a Employee object and add it to the list
 			em.add(buildObject(resultSet));
 		}
+		
+		// Returns the populated list of Employee objects
 		return em;
 	}
 }

34814e5655d28f59b7df1e359bfc091e9a1c4cab
LumiUCN <10632106@ucn.dk>
2025-03-19 12:40:57 +0100

    Update TestSaleOrderDB.java
    Made the test remove the data it added to the system after it's done.

--- src/tests/TestSaleOrderDB.java
+++ src/tests/TestSaleOrderDB.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.sql.SQLException;
 import java.time.LocalDate;
 
@@ -18,7 +20,7 @@ import model.Product;
 import model.SaleOrder;
 import model.SaleOrderLine;
 
-public class TestSaleOrderDB 
+public class TestSaleOrderDB
 {
 	private SaleOrderDB saleOrderDB;
 	private SaleOrder saleOrder;
@@ -26,44 +28,53 @@ public class TestSaleOrderDB
 	private Customer customer;
 	private SaleOrderLine saleOrderLine;
 	private Product p;
-	
-	//opens connection to the database
+	private static int orderId;
+
+	// opens connection to the database
 	@BeforeAll
-	public static void setUp() 
-	{		
+	public static void setUp()
+	{
 		DBConnection.getInstance().getConnection();
 	}
-	
+
 	@BeforeEach
 	public void setUpDB() throws SQLException
 	{
 		saleOrderDB = new SaleOrderDB();
 		saleOrder = new SaleOrder(null, null, false, null, null, false, null);
-		employee = new Employee(10, "John", "Smith", "boss", "11111111", "johnsmith@example.com", "Denmark", "region midtjylland", "Mønsted",8800, "Badstreet", 12, 0, null);
-		customer = new Customer(10, "John", "Doe", "Private", "12121212", "JohnDoe@dead.com", false, "Denmark", "region sydjylland", "Sønderborg", 1121, "Alexgade", 13, 5, "12B");
-		p = new Product(199, "nuclear bomb", 100, 250, "Russia", 10);
+		employee = new Employee(1000, "John", "Smith", "boss", "11111111", "johnsmith@example.com", "Denmark",
+				"region midtjylland", "Mønsted", 8800, "Badstreet", 12, 0, null);
+		customer = new Customer(101, "John", "Doe", "Private", "12121212", "mikkel@example.com", false, "Denmark",
+				"region sydjylland", "Sønderborg", 1121, "Alexgade", 13, 5, "12B");
+		p = new Product(1, "nuclear bomb", 100, 250, "Russia", 10);
 	}
-	
+
 	@AfterAll
 	public static void tearDown()
 	{
-		
+		// Removes the added data 
+		try
+		{
+			new SaleOrderDB().removeSaleOrder(orderId);
+		} catch (Exception e) {}
+
 	}
-	
+
 	@Test
 	public void TestInsertOrder() throws DataAccessException, SQLException
 	{
 		saleOrder.setCustomer(customer);
-		
+
 		saleOrder.setEmployee(employee);
-		
+
 		saleOrderLine = new SaleOrderLine(1, p, 250);
 		saleOrder.addSaleOrderLine(saleOrderLine);
-		
+
 		saleOrder.setCreatedDate(LocalDate.now());
 		saleOrder.setDeliveryDate(LocalDate.now().plusDays(7));
-		saleOrder.setPaymentDate(LocalDate.now().plusDays(30));	
-				
+		saleOrder.setPaymentDate(LocalDate.now().plusDays(30));
+		orderId = saleOrder.getOrderId();
+
 		Assertions.assertDoesNotThrow(() -> saleOrderDB.insertSaleOrderToDatabase(saleOrder));
 	}
 }
